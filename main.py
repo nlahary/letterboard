04fcc7152c852284325dd13908bb127b68f3a446
nlahary <nathanaellahary@gmail.com>
2024-08-21 08:19:46 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,253 @@
+from selectolax.parser import HTMLParser
+import httpx
+import asyncio
+import pandas as pd
+from time import time
+from concurrent.futures import ThreadPoolExecutor
+import backoff
+import streamlit as st
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+import plotly.graph_objects as go
+import numpy as np
+import matplotlib.patches as patches
+
+from visuals import draw_radar_decades, draw_top3, fav_countries, draw_log_timeline, fav_genres, draw_rating_hist, fav_actors, fav_studios, radar_plot
+
+
+def get_total_pages(username: str) -> int:
+    url = f"https://letterboxd.com/{username}/films/diary/"
+
+    response = httpx.get(url, verify=False)
+    response.raise_for_status()
+
+    parser = HTMLParser(response.text)
+    pages = parser.css("li.paginate-page")
+
+    if pages:
+        max_pages = int(pages[-1].text())
+        return max_pages
+
+    return 1
+
+
+@backoff.on_exception(backoff.expo, (httpx.HTTPStatusError, httpx.RequestError), max_tries=5, jitter=None)
+async def fetch_page(client: httpx.AsyncClient, url: str) -> str:
+    """ Fetch a single page of the diary asynchronously """
+    response = await client.get(url, timeout=10.0, )
+    response.raise_for_status()
+    return response.text
+
+
+def parse_content(content: str) -> pd.DataFrame:
+    """ Parse the content of the diary page and return a DataFrame """
+    parser = HTMLParser(content)
+
+    films = parser.css("a[rel='nofollow']")
+
+    films_names = [film.attrs["data-film-name"] for film in films]
+    films_ratings = [film.attrs["data-rating"] for film in films]
+    films_years = [int(film.attrs["data-film-year"]) for film in films]
+    films_liked = [film.attrs["data-liked"] for film in films]
+    films_log_date = [film.attrs["data-viewing-date"] for film in films]
+    films_urls = [
+        film.attrs["data-film-poster"].replace("image-150/", "") for film in films]
+
+    return pd.DataFrame(
+        {"film": films_names, "rating": films_ratings, "date": films_years,
+            "liked": films_liked, "log_date": films_log_date, "url": films_urls}
+    )
+
+
+async def fetch_film_details(client: httpx.AsyncClient, film_url: str, executor: ThreadPoolExecutor) -> dict:
+    """ Fetch the details of a single film asynchronously """
+    full_url = f"https://letterboxd.com{film_url}"
+    content = await fetch_page(client, full_url)
+
+    # Parse the film details in a separate thread
+    loop = asyncio.get_event_loop()
+    details = await loop.run_in_executor(executor, parse_film_details, content)
+
+    return details
+
+
+def parse_film_details(content: str) -> dict:
+    """ Parse the film details from the film page """
+    parser = HTMLParser(content)
+
+    country = parser.css_first("a[href*='/films/country/']").text(
+    ) if parser.css_first("a[href*='/films/country/']") else None
+    studio = parser.css_first(
+        "a[href*='/studio/']").text() if parser.css_first("a[href*='/studio/']") else None
+    primary_language = parser.css_first("a[href*='/films/language/']").text(
+    ) if parser.css_first("a[href*='/films/language/']") else None
+
+    genres = parser.css(
+        "a[href*='/films/genre/']") if parser.css("a[href*='/films/genre/']") else None
+    genres = [genre.text() for genre in genres] if genres else []
+
+    director = parser.css_first(
+        "a[href*='/director/']").text() if parser.css_first("a[href*='/director/']") else None
+
+    actors = parser.css("a[href*='/actor/']") if parser.css(
+        "a[href*='/actor/']") else None
+    actors = [actor.text() for actor in actors] if actors else []
+
+    running_time = parser.css_first("p[class*='text-link']").text() if parser.css_first(
+        "p[class*='text-link']") else None
+    running_time = [char for char in running_time.split()[0]
+                    if char.isdigit()] if running_time else None
+    running_time = int("".join(map(str, running_time))
+                       ) if running_time else None
+
+    # get "content" attribute value from meta tag whose name is "twitter:data2"
+    average_rating = parser.css_first(
+        "meta[name='twitter:data2']").attrs["content"] if parser.css_first("meta[name='twitter:data2']") else None
+    average_rating = float(average_rating.split(
+        " ")[0]) if average_rating else None
+    return {
+        "country": country,
+        "studio": studio,
+        "primary_language": primary_language,
+        "genres": genres,
+        "director": director,
+        "actors": actors,
+        "running_time": running_time,
+        "average_rating": average_rating
+    }
+
+
+async def fetch_data(client: httpx.AsyncClient, username: str, page: int, executor: ThreadPoolExecutor) -> pd.DataFrame:
+    """ Fetch the data for a single page of the diary """
+    url = f"https://letterboxd.com/{username}/films/diary/page/{page}/"
+    content = await fetch_page(client, url)
+
+    loop = asyncio.get_event_loop()
+    df = await loop.run_in_executor(executor, parse_content, content)
+
+    # Fetch film details for each film on the page
+    film_details_tasks = [
+        fetch_film_details(client, film_url, executor)
+        for film_url in df["url"]
+    ]
+    details_list = await asyncio.gather(*film_details_tasks, return_exceptions=True)
+
+    # Attach the details to the DataFrame
+    details_df = pd.DataFrame(details_list)
+    combined_df = pd.concat([df, details_df], axis=1)
+    return combined_df
+
+
+async def main(username: str, total_pages: int) -> pd.DataFrame:
+    async with httpx.AsyncClient() as client:
+        with ThreadPoolExecutor() as executor:
+            tasks = [fetch_data(client, username, page, executor)
+                     for page in range(1, total_pages + 1)]
+            results = await asyncio.gather(*tasks)
+
+    return pd.concat(results, ignore_index=True)
+
+
+def run_app():
+
+    st.title("Letterboard : Your Letterboxd Diary Analysis",)
+
+    # add a description
+    st.markdown(
+        """
+        Welcome to Letterboard! This app allows you to analyze your Letterboxd diary and discover interesting insights about your film-watching habits.
+        Enter your Letterboxd username below and click the 'Fetch Diary' button to get started.
+
+        Inspired by the work of Favour O. ([Link to GitHub](https://github.com/afoshiok/Letterboard))
+
+        Made by [Nathanaël L.]
+        """
+    )
+
+    username = st.text_input("Enter your Letterboxd username:").strip()
+
+    if st.button("Fetch Diary"):
+        if username:
+            try:
+                total_pages = get_total_pages(username)
+            except httpx.HTTPStatusError as e:
+                st.error(
+                    f"User '{username}' not found. Please check the username and try again.")
+                return
+
+            with st.spinner("Fetching data..."):
+                try:
+                    df = asyncio.run(main(username, total_pages))
+
+                except AttributeError as e:
+                    if "PoolTimeoutError" in str(e):
+                        st.error("The request timed out. Please try again.")
+                        return
+
+                tab_level1, tab_level2 = st.tabs(
+                    ["Level.1", "Level.2"])
+
+                with tab_level1:
+                    try:
+                        fig = draw_top3(df)
+                        st.pyplot(fig)
+
+                        fig = fav_countries(df)
+                        st.plotly_chart(fig)
+
+                        fig = draw_log_timeline(df)
+                        st.plotly_chart(fig)
+
+                        fig = fav_genres(df)
+                        st.plotly_chart(fig)
+
+                        fig = draw_rating_hist(df)
+                        st.plotly_chart(fig)
+
+                        fig = fav_actors(df)
+                        st.plotly_chart(fig)
+                    except UnboundLocalError:
+                        st.error(
+                            f"Timeout error. Please try again or check your internet connection.")
+                        return
+                with tab_level2:
+                    # show dataframe
+                    fig = radar_plot(df)
+
+                    # Plot tile :
+                    title = "Your Favorite Studios"
+                    subtitle = """
+                    
+                    The size of the bars represents the average of your ratings for the movies from each studio.
+                    The white dots represent the average rating of the movies from each studio.
+                    The color gradient   represents the number of movies you have watched from each studio.
+
+                    Inspired by the work of [Tobias Stadler](https://tobias-stalder.netlify.app/dataviz/) & [Tomás Capretto](https://tomicapretto.com/)
+                    """
+
+                    # Add title and subtitle to the plot
+
+                    st.markdown(f"""
+                    # {title}
+                    {subtitle}
+                    """)
+
+                    st.pyplot(fig)
+
+                    title = "Your Favorite Decades"
+                    subtitle = " The radar chart below shows the number of movies you have watched per decade (Top 8).\n"
+
+                    st.markdown(f"""
+                    # {title}
+                    {subtitle}
+                    """)
+
+                    fig = draw_radar_decades(df)
+                    st.pyplot(fig)
+        else:
+            st.error("Please enter a username.")
+
+
+if __name__ == "__main__":
+    run_app()
---
+++ visuals.py
@@ -0,0 +1,480 @@
+from matplotlib import pyplot as plt
+import matplotlib.patches as patches
+import pandas as pd
+import numpy as np
+import plotly.graph_objects as go
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+from matplotlib.cm import ScalarMappable
+from matplotlib.lines import Line2D
+from mpl_toolkits.axes_grid1.inset_locator import inset_axes
+from textwrap import wrap
+
+
+def draw_top3(df):
+
+    total_films = df.shape[0]
+    favorite_director = df['director'].mode().values[0]
+    favorite_actor = df['actors'].explode().mode().values[0]
+
+    if len(favorite_director) > 13:
+        favorite_director = "\n"+favorite_director.replace(" ", "\n")
+
+    if len(favorite_actor) > 13:
+        favorite_actor = "\n"+favorite_actor.replace(" ", "\n")
+
+    colors = ['#FF8000', '#00E054', '#40BCF4']
+
+    fig, ax = plt.subplots()
+    fig.patch.set_facecolor('none')
+    ax.patch.set_facecolor('none')
+
+    circles = [
+        {'center': (1, 1), 'radius': 0.5, 'color': colors[0],
+            'text': f'\n {total_films}',
+            'subtext': 'Total Films Logged:',
+            'text_font_size': 10},
+
+        {'center': (1.9, 1), 'radius': 0.5, 'color': colors[1],
+            'text': f'\n {favorite_director}',
+            'subtext': 'Favorite Director:',
+            'text_font_size': 10},
+        {'center': (2.8, 1), 'radius': 0.5, 'color': colors[2],
+            'text': f'\n {favorite_actor}',
+            'subtext': 'Favorite Actor:',
+            'text_font_size': 10}
+    ]
+
+    for circle in circles:
+        ax.add_patch(patches.Circle(
+            circle['center'], circle['radius'], color=circle['color'], alpha=0.5))
+
+        ax.text(circle['center'][0], circle['center'][1], circle['text'],
+                ha='center', va='center', color='white', weight='bold', fontsize=circle['text_font_size'])
+
+        ax.text(circle['center'][0], circle['center'][1] + 0.1, circle['subtext'],
+                ha='center', va='center', fontsize=8, color='white', style='oblique')
+
+    ax.set_aspect('equal')
+
+    plt.xlim(0, 3.5)
+    plt.ylim(0, 2)
+    plt.axis('off')
+
+    return fig
+
+
+def fav_countries(df):
+    # Get the value counts of countries and select only the top 10
+    top_countries = df['country'].value_counts(
+    ).sort_values(ascending=True).tail(5)
+
+    # Define the colors for the gradient
+    colors = ['#FF8000', '#00E054', '#40BCF4', "#272F36"]
+
+    # Create a color palette with a gradient
+    # Since Plotly doesn't directly support gradient color palettes, we manually assign colors
+    color_map = {country: colors[i % len(
+        colors)] for i, country in enumerate(top_countries.index)}
+
+    # Create the horizontal bar plot using Plotly
+    fig = go.Figure()
+
+    fig.add_trace(go.Bar(
+        x=top_countries.values,
+        y=top_countries.index,
+        orientation='h',
+        marker=dict(
+            color=[color_map[country]
+                   for country in top_countries.index],
+            # Thin black border around bars
+            line=dict(color='black', width=1)
+        ),
+        text=[f'{value}' for value in top_countries.values],
+        textposition='outside',
+        width=0.5  # Keep bar width unchanged to avoid altering spacing
+    ))
+
+    # Update the layout to customize appearance and disable interactivity
+    fig.update_layout(
+        title="Distribution of Films by Country (Top 10)",
+        xaxis_title="",
+        yaxis_title="",
+        xaxis=dict(
+            showline=False,
+            showgrid=False,
+            showticklabels=False
+        ),
+        yaxis=dict(
+            showline=False,
+            showgrid=False,
+            showticklabels=True,
+            # Increase the size of the y-axis tick labels
+            tickfont=dict(size=14),
+        ),
+        plot_bgcolor='rgba(0,0,0,0)',
+        paper_bgcolor='rgba(0,0,0,0)',
+        # Adjust margins for better spacing
+        margin=dict(t=50, l=100, r=25, b=50),
+        height=800,  # Increase the height of the figure
+        width=1200,  # Increase the width of the figure
+        clickmode='none',
+        hovermode=False,
+        dragmode=False
+    )
+
+    return fig
+
+
+def draw_log_timeline(df):
+    colors = ['#FF8000', '#00E054', '#40BCF4', "#272F36"]
+    # Convert 'log_date' to datetime and extract the year-month period
+    df['log_date'] = pd.to_datetime(df['log_date'])
+    df['year_month'] = df['log_date'].dt.to_period('M').astype(str)
+
+    # Group by year-month and count the logs
+    log_counts = df.groupby(
+        ['year_month']).size().reset_index(name='counts')
+
+    # Create the bar chart using go.Bar
+    fig = go.Figure(data=[
+        go.Bar(
+            x=log_counts['year_month'],
+            y=log_counts['counts'],
+            marker=dict(
+                color=log_counts['counts'], colorscale=colors)
+        )
+    ])
+
+    # Update the layout to customize appearance and disable the legend
+    fig.update_layout(
+        title="Your Film Logs Over Time",
+        xaxis_title="",
+        yaxis_title="Number of Logs",
+        paper_bgcolor='rgba(0,0,0,0)',
+        plot_bgcolor='rgba(0,0,0,0)',
+        font=dict(color='black'),
+        showlegend=False,  # Ensure the legend is disabled
+        xaxis_tickangle=-45
+    )
+
+    return fig
+
+
+def fav_genres(df):
+    colors = ['#FF8000', '#00E054', '#40BCF4', "#272F36"]
+    # Get the value counts of genres and select only the top 10
+    genres = df['genres'].explode().value_counts().nlargest(10)
+
+    # Create the treemap figure using go.Treemap
+    fig = go.Figure(go.Treemap(
+        labels=genres.index,          # Genre names
+        # Empty string as all items are root nodes
+        parents=[""] * len(genres),
+        values=genres.values,         # Corresponding values
+        marker=dict(colors=genres.values,
+                    colorscale=colors),  # Color settings
+        textinfo="label+value"        # Display label and value on the treemap
+    ))
+
+    # Update the layout to customize appearance and disable the legend
+    fig.update_layout(
+        clickmode='none',
+        hovermode=False,
+        dragmode=False,
+        margin=dict(t=50, l=25, r=25, b=25),
+        title_font_size=20,
+        showlegend=False,  # Ensure the legend is disabled
+        title="Your Most Watched Genres (Top 10)",
+        paper_bgcolor='rgba(0,0,0,0)',  # Transparent background
+        # Transparent plot background
+        plot_bgcolor='rgba(0,0,0,0)',
+        font=dict(color='black')        # Font color for labels
+    )
+
+    return fig
+
+
+def draw_rating_hist(df):
+    df['rating'] = df['rating'].astype(float) / 2
+
+    # Define the bins and labels
+    bins = [0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5]
+    bin_labels = ["0.5", "1", "1.5", "2",
+                  "2.5", "3", "3.5", "4", "4.5", "5"]
+
+    # Compute the histogram
+    hist, _ = np.histogram(df['rating'], bins=bins)
+
+    # Create the bar plot
+    fig = go.Figure()
+
+    fig.add_trace(go.Bar(
+        x=bin_labels,
+        y=hist,
+        marker=dict(color='#FF8000'),
+        text=[f'{count}' for count in hist],
+        textposition='outside'
+    ))
+
+    # Update layout for better appearance
+    fig.update_layout(
+        xaxis=dict(
+            showline=False,
+            showgrid=False,
+            showticklabels=True,
+            ticks='outside'
+        ),
+        yaxis=dict(
+            showline=False,
+            showgrid=True,
+            showticklabels=True,
+            ticks='outside'
+        ),
+        plot_bgcolor='rgba(0,0,0,0)',
+        paper_bgcolor='rgba(0,0,0,0)',
+        margin=dict(t=50, l=50, r=50, b=50),
+        height=600,
+        width=900,
+        title='Distribution of Film Ratings',
+        title_font_size=20
+    )
+    return fig
+
+
+def fav_actors(df):
+    colors = ['#FF8000', '#00E054', '#40BCF4', "#272F36"]
+    actors = df['actors'].explode().value_counts().nlargest(10)
+
+    # Create the treemap figure using go.Treemap
+    fig = go.Figure(go.Treemap(
+        labels=actors.index,          # Actor names
+        # Empty string as all items are root nodes
+        parents=[""] * len(actors),
+        values=actors.values,         # Corresponding values
+        marker=dict(colors=actors.values,
+                    colorscale=colors),  # Color settings
+        textinfo="label+value"        # Display label and value on the treemap
+    ))
+
+    # Update the layout to customize appearance and disable the legend
+    fig.update_layout(
+        clickmode='none',
+        hovermode=False,
+        dragmode=False,
+        margin=dict(t=50, l=25, r=25, b=25),
+        title_font_size=20,
+        showlegend=False,  # Ensure the legend is disabled
+        title="Your Most Watched Actors (Top 10)",
+        paper_bgcolor='rgba(0,0,0,0)',  # Transparent background
+        # Transparent plot background
+        plot_bgcolor='rgba(0,0,0,0)',
+        font=dict(color='black')        # Font color for labels
+    )
+
+    return fig
+
+
+def fav_studios(df):
+    # Get the value counts of studios and select the top 3
+    top_studios = df['studio'].value_counts().nlargest(10)
+
+    # Define colors for the donut chart
+    colors = ['#FF8000', '#00E054', '#40BCF4']
+
+    # Create the donut chart
+    fig = go.Figure()
+
+    fig.add_trace(go.Pie(
+        labels=top_studios.index,
+        values=top_studios.values,
+        hole=0.4,  # Make it a donut chart
+        marker=dict(
+            colors=colors
+        ),
+        textinfo='label',
+        # Change font size based on string length
+        textfont=dict(
+            size=[20 if len(label) <= 10 else
+                  15 if len(label) <= 15 else 10
+                  for label in top_studios.index],
+
+
+            color='black'),
+        showlegend=False
+    ))
+
+    # Update layout for better appearance
+    fig.update_layout(
+        title='Your 10 Favorite Studios',
+        title_font_size=20,
+        plot_bgcolor='rgba(0,0,0,0)',
+        paper_bgcolor='rgba(0,0,0,0)',
+        height=600,
+        width=900,
+        margin=dict(t=50, l=50, r=50, b=50),
+        legend=dict(
+            x=0.8,
+            y=0.5,
+            title='Studios',
+            # Correctly specified as an integer
+            title_font=dict(size=13),
+        ),
+        clickmode='none',
+        hovermode=False,
+        dragmode=False
+    )
+
+    return fig
+
+
+def radar_plot(df):
+
+    studios = df['studio'].value_counts().nlargest(10)
+    studio_ratings = df.groupby('studio')['rating'].mean().loc[studios.index]
+    studio_counts = df['studio'].value_counts().loc[studios.index]
+    studio_avg_ratings = df.groupby(
+        'studio')['average_rating'].mean().loc[studios.index]
+
+    df_studios = pd.DataFrame(
+        {'rating': studio_ratings, 'counts': studio_counts, 'avg_rating': studio_avg_ratings})
+
+    MOVIES_N = df_studios['counts'].values
+
+    COLORS = ["#6C5B7B", "#C06C84", "#F67280", "#F8B195"]
+    COLORS = ['#FF8000', '#00E054', '#40BCF4', "#272F36"]
+    cmap = mpl.colors.LinearSegmentedColormap.from_list(
+        "my color", COLORS, N=256)
+    norm = mpl.colors.Normalize(vmin=MOVIES_N.min(), vmax=MOVIES_N.max())
+    COLORS = cmap(norm(MOVIES_N))
+
+    # Ajuster la largeur des barres pour éviter le chevauchement
+    # Facteur 0.8 pour réduire le chevauchement
+    bar_width = 2 * np.pi / len(df_studios) * 0.8
+
+    ANGLES = np.linspace(0, 2 * np.pi, len(df_studios),
+                         endpoint=False).tolist()
+
+    RATINGS = df_studios['rating'].tolist()
+    AVG_RATINGS = df_studios['avg_rating'].tolist()
+    fig, ax = plt.subplots(figsize=(10, 10), subplot_kw=dict(polar=True))
+
+    fig.patch.set_facecolor('none')
+    ax.patch.set_facecolor('none')
+
+    ax.set_theta_offset(1/2*np.pi / 2)
+    ax.set_ylim(0, 5)
+
+    # Colorier les grilles radiales en blanc
+    ax.grid(color='white')
+
+    # Utiliser la largeur de barre dans la fonction ax.bar
+    ax.bar(ANGLES, RATINGS, color=COLORS, linewidth=0, width=bar_width)
+    ax.vlines(ANGLES, 0, 5, colors='white', ls=(
+        0, (4, 4)), zorder=11, alpha=0.5)
+    ax.scatter(ANGLES, AVG_RATINGS, color='white', s=100, zorder=10)
+
+    # Colorier le dernier cercle en blanc
+    ax.spines['polar'].set_visible(False)
+
+    STUDIOS = ["\n".join(wrap(studio, 10, break_long_words=False))
+               for studio in df_studios.index]
+
+    ax.set_xticks(ANGLES)
+    ax.set_xticklabels(STUDIOS, fontsize=10, fontweight='bold', color='white')
+
+    # Assurer que les grilles radiales sont colorées en blanc et visibles
+    ax.yaxis.grid(True, color='white', linestyle='-', linewidth=1)
+    ax.xaxis.grid(False)
+
+    ax.tick_params(axis='x', colors='white', labelsize=13)
+    ax.tick_params(axis='y', colors='white', labelsize=15)
+
+    XTICKS = ax.xaxis.get_major_ticks()
+    for tick in XTICKS:
+        tick.set_pad(30)
+
+    plt.setp(ax.get_yticklabels(), color='white')
+
+    # Color gradient legend to represent the number of movies : at the bottom of the radar plot, centered
+    gradient = np.linspace(0, 1, 256).reshape(1, -1)
+    gradient = np.vstack((gradient, gradient))
+
+    ax_inset = inset_axes(ax, width="50%", height="3%", loc='lower center',
+                          bbox_to_anchor=(0, -0.3, 1, 1),
+                          bbox_transform=ax.transAxes, borderpad=0)
+
+    ax_inset.imshow(gradient, aspect='auto', cmap=cmap)
+    ax_inset.set_xticks([])
+    ax_inset.set_yticks([])
+    ax_inset.set_frame_on(False)
+
+    min_count = MOVIES_N.min()
+    max_count = MOVIES_N.max()
+    mid_count = (min_count + max_count) // 2  # valeur intermédiaire
+
+    # Position des labels
+    ax_inset.text(0, 1.5, str(min_count), color='white',
+                  fontsize=13, ha='center', va='center', transform=ax_inset.transAxes)
+    ax_inset.text(0.5, 1.5, str(mid_count), color='white', fontsize=13,
+                  ha='center', va='center', transform=ax_inset.transAxes)
+    ax_inset.text(1, 1.5, str(max_count), color='white', fontsize=13,
+                  ha='center', va='center', transform=ax_inset.transAxes)
+
+    # Title of the gradient legend
+    ax_inset.text(0.5, -2, 'Number of Movies', color='white',
+                  fontsize=15, ha='center', va='center', transform=ax_inset.transAxes, fontstyle='italic')
+
+    return fig
+
+
+def draw_radar_decades(df):
+
+    df["decade"] = df["date"] // 10 * 10
+
+    decade_counts = df["decade"].value_counts().sort_index().nlargest(8)
+    YEARS = decade_counts.index.tolist()
+    MOVIES_N = decade_counts.values.tolist()
+
+    ANGLES = np.linspace(0, 2 * np.pi, len(YEARS), endpoint=False).tolist()
+
+    fig, ax = plt.subplots(figsize=(8, 8), subplot_kw=dict(polar=True))
+
+    for i in range(len(YEARS)):
+        ax.plot([ANGLES[i], ANGLES[i]], [0, MOVIES_N[i]],
+                color='orange', linewidth=2)
+        ax.scatter(ANGLES[i], MOVIES_N[i],
+                   color='orange', s=100)
+
+    # Extra circle for the maximum number of movies watched during a decade
+    max_movies_n = max(MOVIES_N)
+    ax.plot(np.linspace(0, 2 * np.pi, 100),
+            [max_movies_n] * 100, linestyle='--', color='#f4e60b', linewidth=1)
+    ax.text(1, max_movies_n + 5,
+            f'{max_movies_n}', color='#f4e60b',
+            fontsize=10, ha='center')
+
+    fig.patch.set_facecolor('none')
+    ax.patch.set_facecolor('none')
+
+    ax.set_ylim(0, max(MOVIES_N) + 15)
+
+    ticks = np.quantile(MOVIES_N, np.linspace(0, 1, 5))
+    # Round the ticks to the nearest decade. For example, 23 -> 20, 3 -> 0, 7 -> 10
+    ticks = np.round(ticks, -1).astype(int)
+
+    ax.set_yticks(ticks)
+    ax.set_yticklabels(ticks, color='white', size=10)
+
+    ax.set_xticklabels(YEARS, size=15, color='white')
+
+    # Set the padding between the ticks and the decade labels
+    XTICKS = ax.xaxis.get_major_ticks()
+    for tick in XTICKS:
+        tick.set_pad(15)
+
+    return fig
